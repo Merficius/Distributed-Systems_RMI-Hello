93cd659f70978b173b62b3c8b699f3ab20e46819
Mariano Portilla <chemanoportilla@hotmail.com>
2021-09-29 15:42:46 -0500

    Add file oferta for auction project

---
+++ hello/Oferta.java
@@ -0,0 +1,13 @@
+package example.hello;
+
+import java.io.Serializable;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Oferta implements Serializable{
+	// Attributes
+
+	// Constructor
+
+	// Methods
+}
